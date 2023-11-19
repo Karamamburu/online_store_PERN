619ba570a4bdbe46f7cfeae07cbc78348b0a417a
Denis Khmyrov <denis.e.khmyrov@gmail.com>
2023-11-19 11:41:13 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,22 @@
+import dotenv from 'dotenv'
+import express from 'express'
+import sequelize from './db.js';
+import * as models from './models.js/models.js'
+
+dotenv.config()
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+const start = async () => {
+  try {
+    await sequelize.authenticate()
+    await sequelize.sync()
+    app.listen(PORT, () => console.log(`Server is running on port ${PORT}`))
+
+  } catch (error) {
+    console.error(error)
+  }
+}
+
+start()
